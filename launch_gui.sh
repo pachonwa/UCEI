2c03671f5c9a2cd7357cd0c19fae41394b440e39
pachonwa <phoebeachonwa.mehs@gmail.com>
2026-04-04 01:35:02 -0700

    Current and previous fuel cell code

---
+++ launch_gui.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Start the OctoPrint Server in the background (&)
+OCTO_PATH="/Users/phoebea/OctoPrint/venv/bin/octoprint"
+GUI_DIR="/Users/phoebea//Desktop/My CSE classes/UCI/M. Eng/"
+
+# Activate virtual environment
+cd "$GUI_DIR"
+source venv/bin/activate
+
+# Check that dependencies are installed
+echo "Checking dependencies..."
+pip install --upgrade pip
+pip install -r requirements.txt
+
+#S tart OctoPrint in the background (if not already running)
+if ! pgrep -f "octoprint" 
+then
+    echo "Starting OctoPrint..."
+    $OCTO_PATH serve --port 5001 &
+    sleep 10 #Wait for the server to fully initialize its API
+fi
+
+# launch  GUI
+python3 -u  gui
